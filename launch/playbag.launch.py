5ce9967ef96b203025daa67051a3582a2a165a8a
rick <you@example.com>
2022-11-11 12:54:41 +0100

    first commit

---
+++ launch/playbag.launch.py
@@ -0,0 +1,27 @@
+
+import os, launch 
+
+from launch import LaunchDescription, Substitution, LaunchContext
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, LogInfo, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir, PythonExpression
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
+from typing import Text
+from ament_index_python.packages import get_package_share_directory
+
+
+
+def generate_launch_description():
+    return LaunchDescription([
+
+
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', 'rosbag2_2022_11_03-13_03_34'],
+            output='screen',
+            #remappings=remappings_bag
+            ),
+            
+        #TODO fix to republish     
+
+    ])
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os 
+from glob import glob
+
+package_name = 'my_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rick',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_package.my_node:main'
+        ],
+    },
+)

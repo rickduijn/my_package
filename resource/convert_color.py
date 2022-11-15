cfa8dbe718ce410dd1e93619d018301722c75551
rick <you@example.com>
2022-11-15 16:26:33 +0100

    New option to save images

--- resource/convert_color.py
+++ resource/convert_color.py
@@ -7,7 +7,7 @@
 # Import the necessary libraries
 import rclpy # Python library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
-from sensor_msgs.msg import Image, _compressed_image # Image is the message type
+from sensor_msgs.msg import Image, CompressedImage # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
  
@@ -25,10 +25,11 @@ class ImageSubscriber(Node):
     # Create the subscriber. This subscriber will receive an Image
     # from the video_frames topic. The queue size is 10 messages.
     self.subscription = self.create_subscription(
-      Image, 
+      #Image, 
+      CompressedImage,
       #'video_frames', 
-      'right/image_rect', # Thi works 
-      #'stereo/depth/compressed',
+      #'right/image_rect', # Thi works 
+      'color/image/compressed',
       self.listener_callback, 
       10)
     self.subscription # prevent unused variable warning
@@ -44,10 +45,14 @@ class ImageSubscriber(Node):
     self.get_logger().info('Receiving video frame')
  
     # Convert ROS Image message to OpenCV image
-    current_frame = self.br.imgmsg_to_cv2(data)
-    
+    #current_frame = self.br.imgmsg_to_cv2(data)
+    current_frame = self.br.compressed_imgmsg_to_cv2(data)
+
     # Display image
     cv2.imshow("camera", current_frame)
+
+    # Use this for saving one random image 
+    #cv2.imwrite("image.jpeg", current_frame)
     
     cv2.waitKey(1)
   
